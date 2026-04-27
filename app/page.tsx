71fc7ab42ad75602a1e4974d19c584eaa54fe358
edwinirungu <irungundigirigi@gmail.com>
2026-04-27 15:27:59 +0300

    feature: add order filtering by status_id

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import useSWR, { useSWRConfig } from "swr";
+import data from "../store/order.json";
 import { useState, useCallback, useRef } from "react";
 import fetcher from "@/lib/utils";
 import CompanyCard from "@/components/Tab";
@@ -10,15 +11,20 @@ import {
   InputGroupInput,
 } from "@/components/ui/input-group";
 import OrderTable from "@/components/OrderTable";
+import { OrderJSON, OrderStatusJSON } from "@/types/order";
 
 export default function Home() {
   const { mutate } = useSWRConfig();
-  const { data, isLoading, isValidating, error } = useSWR("accounts", fetcher);
+  // const { data, isLoading, isValidating, error } = useSWR("accounts", fetcher);
   const [searchInput, setSearchInput] = useState<string>("");
   const handleForm = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setSearchInput(e.target.value);
   }, []);
+
+  const filtered_orders = data.items?.filter((order) =>
+    order.order_status_id.toLowerCase().includes(searchInput.toLowerCase()),
+  );
 
   return (
     <div className="flex flex-col gap-2 m-4">
@@ -37,19 +43,16 @@ export default function Home() {
       </div>
       <div>
         {" "}
-        <OrderTable />{" "}
+        <OrderTable orders={filtered_orders} />{" "}
       </div>
 
       <div className="flex flex-row flex-wrap gap-5 ">
-        {isLoading && <h1>Loading accounts..</h1>}
-        {isValidating && <h1>validating..</h1>}
+        {/* {isLoading && <h1>Loading accounts..</h1>}
+        {isValidating && <h1>validating..</h1>} */}
 
         {/* {error && <div>{error}</div>}
         {data &&
-          data
-            .filter((company) =>
-              company.name.toLowerCase().includes(searchInput.toLowerCase()),
-            )
+          
             .map((company) => (
               <CompanyCard key={company.id} company={company} />
             ))} */}
