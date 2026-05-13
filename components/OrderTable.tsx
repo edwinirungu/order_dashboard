eb2067c821d934ee5419654d14373614cd890e8a
edwinirungu <irungundigirigi@gmail.com>
2026-05-13 12:32:49 +0300

    feature: use shadcn collapsible rows

--- components/OrderTable.tsx
+++ components/OrderTable.tsx
@@ -4,6 +4,12 @@ import StatusBadge from "./StatusBadge";
 import { Separator } from "./ui/separator";
 import { OrderStatusJSON } from "@/types/order";
 import { formatDate, timeDifference } from "@/lib/utils";
+import { Card, CardContent } from "./ui/card";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
 
 export default function OrderTable({ orders }: { orders: OrderJSON[] | [] }) {
   type toggleView = number | null;
@@ -12,92 +18,49 @@ export default function OrderTable({ orders }: { orders: OrderJSON[] | [] }) {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className="grid  grid-cols-6 bg-gray-100 py-1 my-1">
-=======
-      <div className="grid grid-cols-[0.5fr_1fr_2fr_1fr_1fr_1fr_1fr] bg-gray-100 dark:bg-zinc-950 dark:text-white-100 py-1 my-1">
-        <div>ID</div>
->>>>>>> 26849a1caffe32c997930506bf318e27e803c130
-        <div>Details</div>
-        <div>Route</div>
-        <div>Status</div>
-        <div>Vehicle</div>
-        <div>Amount</div>
-        <div>Load</div>
-      </div>
-      <div className="border rounded-sm">
-        {orders?.map((order) => (
-          <div key={order.id}>
-            <div
-              onClick={() => {
-                setOpenOrder(openOrder === order.id ? null : order.id);
-              }}
-<<<<<<< HEAD
-              className="grid text-gray-700 grid-cols-6 gap-1 border-t items-center"
-=======
-              className="grid grid-cols-[0.5fr_1fr_2fr_1fr_1fr_1fr_1fr] p-1 gap-1 border-t items-center"
->>>>>>> 26849a1caffe32c997930506bf318e27e803c130
-            >
-              <span className="font-medium">{order.id}</span>
-              <div>
-                <div className="flex flex-col ">
-                  {formatDate(order.created_at)}
-                </div>
-              </div>
-              <div className="flex flex-col">
-                {order.order_origins[0].address.split(",")[0]} to{" "}
-                {order.order_destinations[0].address.split(",")[0]}
-              </div>
-<<<<<<< HEAD
-              <div className="">
-=======
-              <div className="flex flex-col w-[150px] ">
->>>>>>> 26849a1caffe32c997930506bf318e27e803c130
-                <StatusBadge
-                  order_status={order.order_status_id as OrderStatusJSON}
-                />
-              </div>
-              <div className="flex flex-col">
-                <div>
-                  {order.vehicle ? order.vehicle.licence_plate : "----"}
-                </div>
-                <div> {order.driver?.phone}</div>
-              </div>
-              <div>KES {Number(order.total_price).toFixed(2)}</div>
-              <div>{order.order_loads[0].commodity}</div>
-            </div>
-            {openOrder === order.id && (
-              <div
-                onClick={() => {
-                  setOpenOrder(openOrder === order.id ? null : order.id);
-                }}
-                className="min-height-4 p-2"
-              >
-                <Separator />
-                <div className="flex flex-row gap-6">
-                  <div className="flex flex-col">
-                    <span>Created at : {formatDate(order.created_at)} </span>
-                    <span className="text-green-400">
-                      Dropoff in{" "}
-                      {timeDifference(order.order_destinations[0].dropoff_date)}{" "}
-                      days.
-                    </span>
-                    <span>{order.order_lines[0].name}</span>
-                    <span>Shipper: {order.shipper.name}</span>
-                    <span>Phone: {order.shipper.phone}</span>
-                    <span>Email: {order.shipper.name}</span>
+      {orders?.map((order) => (
+        <Card key={order.id} className="w-full">
+          <CardContent>
+            <Collapsible>
+              <CollapsibleTrigger>
+                <div
+                  onClick={() => {
+                    setOpenOrder(openOrder === order.id ? null : order.id);
+                  }}
+                  className="grid grid-cols-[0.5fr_1fr_2fr_1fr_1fr_1fr_1fr] p-1 gap-1 border-t items-center"
+                >
+                  <span className="font-medium">{order.id}</span>
+                  <div>
+                    <div className="flex flex-col ">
+                      {formatDate(order.created_at)}
+                    </div>
                   </div>
                   <div className="flex flex-col">
-                    <span>Driver: {order.driver?.name}</span>
-                    <span>Phone: {order.driver?.phone}</span>
-                    <span>Email: {order.driver?.email}</span>
+                    {order.order_origins[0].address.split(",")[0]} to{" "}
+                    {order.order_destinations[0].address.split(",")[0]}
                   </div>
+                  <div className="flex flex-col w-[150px] ">
+                    <StatusBadge
+                      order_status={order.order_status_id as OrderStatusJSON}
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <div>
+                      {order.vehicle ? order.vehicle.licence_plate : "----"}
+                    </div>
+                    <div> {order.driver?.phone}</div>
+                  </div>
+                  <div>KES {Number(order.total_price).toFixed(2)}</div>
+                  <div>{order.order_loads[0].commodity}</div>
                 </div>
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
+              </CollapsibleTrigger>
+              <CollapsibleContent>
+                {order.order_destinations[0].address}
+              </CollapsibleContent>
+            </Collapsible>
+          </CardContent>
+        </Card>
+      ))}
     </>
   );
 }
--- components/StatusBadge.tsx
+++ components/StatusBadge.tsx
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { OrderStatusJSON } from "@/types/order";
-
-export default function StatusBadge({
-  order_status,
-}: {
-  order_status: OrderStatusJSON;
-}) {
-  function getBadgeStyle() {
-    const base =
-      "inline-flex items-center text-center px-2.5 py-0.5 text-[16px] rounded-[5px]";
-
-    switch (order_status) {
-      case "delivered":
-        return `${base} bg-[5da271]-50 border border-[#cfe1b9] text-[#5da271]`;
-
-      case "created":
-      case "finalizing_details":
-      case "driver_assigned":
-      case "matching_carriers":
-        return `${base} bg-orange-50 border bg-orange-50 text-orange-500`;
-
-      case "cancelled":
-        return `${base} border bg-red-100 bg-red-50 text-red-500`;
-
-      default:
-        return `${base} bg-gray-50 text-gray-500`;
-    }
-  }
-
-  return <span className={getBadgeStyle()}>{order_status}</span>;
-=======
 import { OrderStatusString } from "@/lib/utils";
 export default function StatusBadge({ order_status }) {
   function getBadgeStyle() {
@@ -63,5 +31,4 @@ export default function StatusBadge({ order_status }) {
       {OrderStatusString(order_status)}
     </div>
   );
->>>>>>> 26849a1caffe32c997930506bf318e27e803c130
 }
